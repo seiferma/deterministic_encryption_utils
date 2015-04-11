6826467283abf97cbcb0f49a924fb1f07c17b860
Stephan Seifermann <seiferma@users.noreply.github.com>
2015-04-11 13:25:44 +0200

    Deploy with lowest python version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='encviewfuse.commons',
-    version='0.2.5',
+    version='0.2.6',
     description='Common encryption utilities for the implementation of simple encryption/decryption views.',
     long_description=long_description,
     url='https://github.com/seiferma/encviewfuse.commons',
