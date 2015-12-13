a747da3cd6daf39b0c26d4d497725e8863af1dd1
Stephan Seifermann <seiferma@users.noreply.github.com>
2015-12-13 21:06:04 +0100

    Replaced travis deployment with custom script.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='deterministic_encryption_utils',
-    version='0.3.0',
+    version='0.3.0.post1',
     description='Common encryption utilities for the implementation of simple encryption/decryption views.',
     long_description=long_description,
     url='https://github.com/seiferma/deterministic_encryption_utils',
